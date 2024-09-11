32a116b0f8f6bd378ac608ab605ecf1c68a5c6cd
Székely Áron <aron20010103@gmail.com>
2024-09-11 17:49:14 +0200

    Hibak javitasa
    Hibak kommentelese

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
+    int *b = new int[NELEMENTS]; // Konstans nev eliras
     std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    for (int i = 0;) //for ciklus feltetele nincs befejezve, 1-tol kell indulnia
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // ertek nincs a string moge kiirva, sor nincs lezarva
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; //nincs kinullazva a valtozo
+    for (int i = 0; i < N_ELEMENTS, i++) // ; kene , helyett
     {
-        atlag += b[i]
+        atlag += b[i] //hianyzo pontosvesszo
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
