1b26349bb0a99c3c1ff82a85e7dc4b1495d3aef1
Székely Áron <aron20010103@gmail.com>
2024-09-11 18:06:28 +0200

    Hibak javitasa 2
    Hibak javitva

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; // Konstans nev eliras
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;) //for ciklus feltetele nincs befejezve, 1-tol kell indulnia
+    int *b = new int[N_ELEMENTS]; // Konstans nev eliras
+    std::cout << "1-100 ertekek duplazasa"<<std::endl;
+    for (int i = 1; i <= N_ELEMENTS; i++) //for ciklus feltetele nincs befejezve, 1-tol kell indulnia
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 1; i <= N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:" // ertek nincs a string moge kiirva, sor nincs lezarva
-    }    
+        std::cout << "Ertek:" << b[i]<<std::endl;// ertek nincs a string moge kiirva, sor nincs lezarva
+    }
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; //nincs kinullazva a valtozo
-    for (int i = 0; i < N_ELEMENTS, i++) // ; kene , helyett
+    int atlag = 0; //nincs kinullazva a valtozo
+    for (int i = 1; i < N_ELEMENTS; i++) // ; kene , helyett
     {
-        atlag += b[i] //hianyzo pontosvesszo
+        atlag += b[i]; //hianyzo pontosvesszo
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
